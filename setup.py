63501b99d019a3363112e01112594572dd0a5e12
Erik <erik@montnemery.com>
2020-09-03 20:50:33 +0200

    Fix package generation

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ long_description = open("README.md").read()
 
 setup(
     name="HATasmota",
-    version="0.0.2",
+    version="0.0.3",
     license="MIT",
     url="https://github.com/emontnemery/hatasmota",
     author="",
     author_email="",
     description="Python module to help parse and construct Tasmota MQTT messages.",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
@@ -25,4 +26,5 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    python_requires='>=3.6',
 )
