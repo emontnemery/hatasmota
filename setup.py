4c436b576334acf62b4002324e484187a0d55383
Erik Montnemery <erik@montnemery.com>
2020-10-26 11:14:29 +0100

    Bump version to 0.0.23

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open("README.md").read()
 
 setup(
     name="HATasmota",
-    version="0.0.22",
+    version="0.0.23",
     license="MIT",
     url="https://github.com/emontnemery/hatasmota",
     author="",
