d00b7420dbd1ae9c7b13ce17731af8f251be1b35
Erik Montnemery <erik@montnemery.com>
2020-11-11 12:44:11 +0100

    Return non naive datetime from last restart time sensor (#42)

--- hatasmota/status_sensor.py
+++ hatasmota/status_sensor.py
@@ -2,7 +2,7 @@
 import asyncio
 import json
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import attr
 
@@ -208,7 +208,7 @@ class TasmotaStatusSensor(TasmotaAvailability, TasmotaEntity):
                 if self._cfg.sensor in SINGLE_SHOT:
                     asyncio.create_task(self._unsubscribe_state_topics())
                 if self._cfg.sensor == SENSOR_STATUS_LAST_RESTART_TIME:
-                    state = datetime.utcnow() - timedelta(seconds=int(state))
+                    state = datetime.now(timezone.utc) - timedelta(seconds=int(state))
                 self._on_state_callback(state)
 
         availability_topics = self.get_availability_topics()
