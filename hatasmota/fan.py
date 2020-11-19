9b58d4367c9d79bcf2774c25c785005c977daebe
Erik Montnemery <erik@montnemery.com>
2020-11-19 19:19:17 +0100

    Add support for fans (#46)
    * Add support for fans
    
    * Tweak

--- hatasmota/fan.py
+++ hatasmota/fan.py
@@ -1,9 +1,17 @@
-"""Tasmota switch."""
+"""Tasmota fan."""
 import logging
 
 import attr
 
-from .const import COMMAND_POWER, CONF_MAC, CONF_OPTIONS, OPTION_HASS_LIGHT
+from .const import (
+    COMMAND_FANSPEED,
+    CONF_DEVICENAME,
+    CONF_MAC,
+    FAN_SPEED_HIGH,
+    FAN_SPEED_LOW,
+    FAN_SPEED_MEDIUM,
+    FAN_SPEED_OFF,
+)
 from .entity import (
     TasmotaAvailability,
     TasmotaAvailabilityConfig,
@@ -11,40 +19,36 @@ from .entity import (
     TasmotaEntityConfig,
 )
 from .utils import (
-    config_get_friendlyname,
     config_get_state_offline,
     config_get_state_online,
-    config_get_state_power_off,
-    config_get_state_power_on,
-    get_state_power,
     get_topic_command,
     get_topic_command_state,
     get_topic_stat_result,
     get_topic_tele_state,
     get_topic_tele_will,
+    get_value_by_path,
 )
+
+SUPPORTED_FAN_SPEEDS = [FAN_SPEED_OFF, FAN_SPEED_LOW, FAN_SPEED_MEDIUM, FAN_SPEED_HIGH]
 
 _LOGGER = logging.getLogger(__name__)
 
 
 @attr.s(slots=True, frozen=True)
-class TasmotaRelayConfig(TasmotaAvailabilityConfig, TasmotaEntityConfig):
-    """Tasmota relay configuation."""
+class TasmotaFanConfig(TasmotaAvailabilityConfig, TasmotaEntityConfig):
+    """Tasmota fan configuation."""
 
     command_topic: str = attr.ib()
-    is_light: bool = attr.ib()
     result_topic: str = attr.ib()
-    state_power_off: str = attr.ib()
-    state_power_on: str = attr.ib()
     state_topic: str = attr.ib()
 
     @classmethod
-    def from_discovery_message(cls, config, idx, platform):
+    def from_discovery_message(cls, config, platform):
         """Instantiate from discovery message."""
         return cls(
-            endpoint="relay",
-            idx=idx,
-            friendly_name=config_get_friendlyname(config, platform, idx),
+            endpoint="fan",
+            idx="ifan",
+            friendly_name=config[CONF_DEVICENAME],
             mac=config[CONF_MAC],
             platform=platform,
             poll_payload="",
@@ -53,16 +57,13 @@ class TasmotaRelayConfig(TasmotaAvailabilityConfig, TasmotaEntityConfig):
             availability_offline=config_get_state_offline(config),
             availability_online=config_get_state_online(config),
             command_topic=get_topic_command(config),
-            is_light=config[CONF_OPTIONS][OPTION_HASS_LIGHT] == 1,
             result_topic=get_topic_stat_result(config),
-            state_power_off=config_get_state_power_off(config),
-            state_power_on=config_get_state_power_on(config),
             state_topic=get_topic_tele_state(config),
         )
 
 
-class TasmotaRelay(TasmotaAvailability, TasmotaEntity):
-    """Representation of a Tasmota relay."""
+class TasmotaFan(TasmotaAvailability, TasmotaEntity):
+    """Representation of a Tasmota fan."""
 
     def __init__(self, **kwds):
         """Initialize."""
@@ -75,11 +76,9 @@ class TasmotaRelay(TasmotaAvailability, TasmotaEntity):
 
         def state_message_received(msg):
             """Handle new MQTT state messages."""
-            state = get_state_power(msg.payload, self._cfg.idx)
-            if state == self._cfg.state_power_on:
-                self._on_state_callback(True)
-            elif state == self._cfg.state_power_off:
-                self._on_state_callback(False)
+            fanspeed = get_value_by_path(msg.payload, [COMMAND_FANSPEED])
+            if fanspeed in SUPPORTED_FAN_SPEEDS:
+                self._on_state_callback(fanspeed)
 
         availability_topics = self.get_availability_topics()
         topics = {
@@ -105,10 +104,10 @@ class TasmotaRelay(TasmotaAvailability, TasmotaEntity):
         """Unsubscribe to all MQTT topics."""
         self._sub_state = await self._mqtt_client.unsubscribe(self._sub_state)
 
-    def set_state(self, state):
-        """Turn the relay on or off."""
-        payload = self._cfg.state_power_on if state else self._cfg.state_power_off
-        command = f"{COMMAND_POWER}{self._cfg.idx+1}"
+    def set_speed(self, fanspeed):
+        """Set the fan's speed."""
+        payload = fanspeed
+        command = COMMAND_FANSPEED
         self._mqtt_client.publish(
             self._cfg.command_topic + command,
             payload,
