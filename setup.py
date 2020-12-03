dd07c0073fd3248079e033bfdaf309d49bf7a712
Erik Montnemery <erik@montnemery.com>
2020-12-03 18:33:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open("README.md").read()
 
 setup(
     name="HATasmota",
-    version="0.1.2",
+    version="0.1.3",
     license="MIT",
     url="https://github.com/emontnemery/hatasmota",
     author="",
