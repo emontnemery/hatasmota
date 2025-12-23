c783a1508d5a8cfb81be49335045430674cfd9a7
Fabian Seitz <seitzf1@yahoo.de>
2025-12-23 23:52:54 +0100

    publish update commands

--- hatasmota/__init__.py
+++ hatasmota/__init__.py
@@ -1,1 +1,9 @@
 """HATasmota."""
+from .const import COMMAND_UPGRADE
+from .update import TasmotaUpdate, TasmotaUpdateConfig
+
+__all__ = [
+    "COMMAND_UPGRADE",
+    "TasmotaUpdate",
+    "TasmotaUpdateConfig",
+]
